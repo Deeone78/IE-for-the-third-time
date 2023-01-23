cb208e119eb1dc331acff15035eee09eca7eb79e
Deeone78 <deeyimark@gmail.com>
2023-01-23 14:51:39 +0000

    more cam stuff

--- Assets/scripts/sghoot.cs
+++ Assets/scripts/sghoot.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class sghoot : MonoBehaviour
+
 {
+   public GameObject CMFreeLook;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class sghoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            CMFreeLook.SetActive(false);
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+
+            CMFreeLook.SetActive(true);
+
+            //dust.Pause();
+            //dust.Clear();
+        }
     }
 }
