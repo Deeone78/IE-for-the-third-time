e58ce98b1017b5f31189f949480ebd60e83e01e7
Deeone78 <deeyimark@gmail.com>
2023-02-04 22:37:10 +0000

    lots of shader stuff

--- Assets/DissolveEmissionShader/DissolveSphere.cs
+++ Assets/DissolveEmissionShader/DissolveSphere.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DissolveSphere : MonoBehaviour {
+public class DissolveSphere : MonoBehaviour
+{
 
+    RaycastHit hit;
+    public float range = 5f;
     Material mat;
 
-    private void Start() {
+    private void Start()
+    {
         mat = GetComponent<Renderer>().material;
+
     }
 
-    private void Update() {
-        mat.SetFloat("_DissolveAmount", Time.time*0.2f);
+    private void Update()
+    {
+
+        Vector3 direction = Vector3.forward;
+        Ray theRay = new Ray(transform.position, transform.TransformDirection(direction * range));
+        if (hit.collider.tag == ("NPC"))
+        {
+            mat.SetFloat("_DissolveAmount", Time.time * 0.2f);
+            Debug.Log("you hit something");
+
+        }
     }
 }
