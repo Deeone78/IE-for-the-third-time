b89828ede8efd71f018acde4caba07055b990118
Deeone78 <deeyimark@gmail.com>
2023-02-06 22:50:38 +0000

    sTFUFF

--- Assets/DissolveEmissionShader/DissolveSphere.cs
+++ Assets/DissolveEmissionShader/DissolveSphere.cs
@@ -2,32 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DissolveSphere : MonoBehaviour
-{
+public class DissolveSphere : MonoBehaviour {
 
-    RaycastHit hit;
-    public float range = 5f;
-    public Material mat;
+    Material mat;
 
-    private void Start()
-    {
+    private void Start() {
         mat = GetComponent<Renderer>().material;
-
+      //  mat.SetFloat("_DissolveAmount", Mathf.Sin(Time.time) / 2 + 0.5f);
     }
 
-    private void Update()
-    {
-        /*
-        Vector3 direction = Vector3.forward;
-        Ray theRay = new Ray(transform.position, transform.TransformDirection(direction * range));
-        if (hit.collider.tag == ("NPC"))
-        {
-            hit.collider.GetComponent<Material>().SetFloat("_DissolveAmount", Time.time * 0.2f);
-            //mat.SetFloat("_DissolveAmount", Time.time * 0.2f);
-            Debug.Log("you hit something");
-
-        }
-        */
+    private void Update() {
+        mat.SetFloat("_DissolveAmount", Mathf.Sin(Time.time) / 2 + 0.5f);
     }
-
 }
