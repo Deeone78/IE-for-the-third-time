fdb024234bc63d88f5764725d9e6e6a1f61de4e0
Deeone78 <deeyimark@gmail.com>
2023-02-07 20:23:45 +0000

    POPO aDDED

--- Assets/scripts/EnermyFollow.cs
+++ Assets/scripts/EnermyFollow.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine AI;
+using UnityEngine.AI;
 
 public class EnermyFollow : MonoBehaviour
 {
 
-  //  public NavMeshAgent enemy;
-  //  public Transform Player;
+    public NavMeshAgent Mob;  
+    public GameObject Player;
+    public float MobDistanceRun = 4.0f;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
+        Mob = GetComponent<NavMeshAgent>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-       // enemy.SetDestination(Player.postion);
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        if (distance < MobDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+            Mob.SetDestination(newPos);
+        }
+
+        // enemy.SetDestination(Player.postion);
     }
 }
